82cc07e4501d1ad999d52b3e968131f819a58cf4
BanLowi <lowi40k@gmail.com>
2025-11-04 22:27:03 +0100

    footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,62 @@
+import FooterBgLogo from "../assets/img/dc-logo-bg.png";
+
 export default function Footer() {
 
-    return
+    return (
+        <footer>
+
+            <div className="container d-flex py-5">
+
+                <div className="row text-white">
+                    <div className="col-3">
+                        <h4>DC COMICS</h4>
+                        <ul class="list-unstyled">
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                    </div>
+                    <div className="col-3">
+                        <h4>DC</h4>
+                        <ul className="list-unstyled">
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy(New)</li>
+                            <li>Ad Choises</li>
+                            <li>Advertisong</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshop</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div className="col-3">
+                        <h4>SITES</h4>
+                        <ul className="list-unstyled">
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC PowerVisa</li>
+                        </ul>
+                    </div>
+                    <div className="col-12">
+                        <h4>SHOP</h4>
+                        <ul className="list-unstyled">
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+
+        </footer>
+    )
 }
