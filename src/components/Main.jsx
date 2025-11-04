477301fa5bead46dd7c9cb2623abbe3438a697da
BanLowi <lowi40k@gmail.com>
2025-11-04 21:13:48 +0100

    main banners

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,42 @@
+import DigitalComicsImg from "../assets/img/buy-comics-digital-comics.png";
+import MerchandiseImg from "../assets/img/buy-comics-merchandise.png";
+import ShopLocatorImg from "../assets/img/buy-comics-shop-locator.png";
+import SubscriptionsImg from "../assets/img/buy-comics-subscriptions.png";
+import VisaImg from "../assets/img/buy-dc-power-visa.svg";
+
 export default function Main() {
 
     return (
         <main>
+
             <section className="bg-dark text-white">
                 <h1 className="container py-5 mb-0">--&gt; Content goes here &lt;--</h1>
             </section>
+
             <section className="bg-primary text-white">
                 <div className="container">
 
-                    <div className="row justify-content-evenly">
-                        <div className="col-2">maial</div>
-                        <div className="col-2">maial</div>
-                        <div className="col-2">maial</div>
-                        <div className="col-2">maial</div>
-                        <div className="col-2">maial</div>
+                    <div className="row justify-content-evenly align-items-center py-5">
+                        <div className="col-2">
+                            <img src={DigitalComicsImg} alt="" width={50} />
+                            <span>DIGITAL COMICS</span>
+                        </div>
+                        <div className="col-2">
+                            <img src={MerchandiseImg} alt="" width={50} />
+                            <span>DC MERCHANDISE</span>
+                        </div>
+                        <div className="col-2">
+                            <img src={SubscriptionsImg} alt="" width={50} />
+                            <span>SUBSCRIPTION</span>
+                        </div>
+                        <div className="col-2">
+                            <img src={ShopLocatorImg} alt="" width={35} />
+                            <span>COMIC SHOP LOCATOR</span>
+                        </div>
+                        <div className="col-2">
+                            <img src={VisaImg} alt="" width={50} />
+                            <span>DC POWER VISA</span>
+                        </div>
                     </div>
 
                 </div>
