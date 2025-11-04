493aa832978eec7f7110abefc338c186421d2d9d
BanLowi <lowi40k@gmail.com>
2025-11-04 15:43:19 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import logo from '../assets/img/dc-logo.png'
+
+export default function Header() {
+
+    return (
+        <header>
+            <img src={logo} alt="" />
+            <nav>
+                <a href="">CHARACTERS</a>
+                <a href="">COMICS</a>
+                <a href="">MOVIES</a>
+                <a href="">TV</a>
+                <a href="">GAMES</a>
+                <a href="">COLLECTIBLES</a>
+                <a href="">VIDEOS</a>
+                <a href="">FANS</a>
+                <a href="">NEWS</a>
+                <a href="">SHOP</a>
+                <a href=""></a>
+            </nav>
+        </header>
+    )
+}
