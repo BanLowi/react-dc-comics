34127d39d1d0b17c2cd91e04e11776e7b4232dec
BanLowi <lowi40k@gmail.com>
2025-11-05 12:59:03 +0100

    catalogue component working grid

--- src/components/Catalogue.jsx
+++ src/components/Catalogue.jsx
@@ -59,7 +59,7 @@ const comics = [
         id: 5,
         title: "Batman #56",
         description: "The Dark Knight's looking to drop both the hammer and sickle on the KGBeast. The Russian super-assassin has gone too far, and Batman will stop at nothing to hunt him down. But is the Dark Knight willing to step into the darkness himself to find justice?",
-        thumb: "https://imgs.search.brave.com/3vP5d3cOVOuC0f6Uhm7CkV_qFk5hGj5rE4jK5I6n7dI/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jZG4y/LnBlbmd1aW4uY29t/LmF1L2NvdmVycy9v/cmlnaW5hbC83OTYx/OTQzMTM5OTk4Lmpw/Zw",
+        thumb: "https://imgs.search.brave.com/jgxYlrx442aozemzjqdmCsd9DkIkRollJp1T8sG8TfE/rs:fit:720:1106:1/g:ce/aHR0cHM6Ly9veXN0/ZXIuaWduaW1ncy5j/b20vd29yZHByZXNz/L3N0Zy5pZ24uY29t/LzIwMTgvMTAvU1RM/MDk3MDk1LTcyMHgx/MTA2LmpwZw",
         price: "$3.99",
         series: "Batman",
         sale_date: "2018-10-03",
@@ -163,11 +163,18 @@ export default function Catalogue() {
                 {
                     comics.map(comic => (
                         <div key={comic.id} className="col-2">
-                            <div className="card">
-                                <img src={comic.thumb} className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <p className="card-text">{comic.series}</p>
+                            <div className="card bg-transparent border-0 rounded-0">
+
+                                <div className="card-header border-0">
+                                    <div className="img_box">
+                                        <img src={comic.thumb} className="img-fluid" alt="..." />
+                                    </div>
                                 </div>
+
+                                <div className="card-body">
+                                    <p className="card-text text-white">{comic.series.toUpperCase()}</p>
+                                </div>
+
                             </div>
                         </div>
                     ))
